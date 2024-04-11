1c9abbea62e1998b147c0c2d8c7bb0027dc54844
ashish <1508ramashish@gmail.com>
2024-04-11 13:06:17 +0530

    iii

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ app.get('/info', (req,res)=>{
 app.get('/way',(req,res)=>{
   res.send({"res.send":"true","res.json":"true","return res.json":"true"})
 })
+app.use('/api',require('./routes/default'))
 // Available Routes ksi aur folder se rout ko lekar hit kar rah hua
 // app.use(route,location) //to hit rote from other file
-app.use('/api/auth', require('./routes/auth'))// require(location of file where route is presnt)
+// app.use('/api/auth', require('./routes/auth'))// require(location of file where route is presnt)
 // app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
--- routes/default.js
+++ routes/default.js
@@ -1,11 +1,11 @@
 const express=require('express')
 const router=express.Router();
-router.get('', (req,res)=>{
+router.get('/', (req,res)=>{
     a="this is a dafult page ";
     // console.log(req.body)
     // res.send(req.body)
     // console.log(req.body)
     // res.json(req.body? req.body :a)
-    res.json(a)
+    res.json({"app-use par request":"default page me "})
 })
 module.exports=router
